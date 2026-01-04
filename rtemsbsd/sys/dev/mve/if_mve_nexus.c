8a87c04d43c0bf4b65a2cb845c2a4b32c0d8e89d
Jeremy Lorelli <lorelli@slac.stanford.edu>
2026-01-03 22:07:40 -0800

    rtemsbsd: Move SIO_RTEMS_SHOW_STATS to rtems_mii_ioctl.h
    Previously this was being defined in each source file

--- rtemsbsd/sys/dev/mve/if_mve_nexus.c
+++ rtemsbsd/sys/dev/mve/if_mve_nexus.c
@@ -86,6 +86,7 @@
 #include <dev/mii/mii.h>
 #include <dev/mii/miivar.h>
 #include <rtems/bsd/local/miibus_if.h>
+#include <rtems/rtems_mii_ioctl.h>
 #include <stdio.h>
 #include <bsp/mv643xx_eth.h>
 #include <bsp.h>
@@ -156,7 +157,6 @@
 /* Hacks -- FIXME */
 rtems_id
 rtems_bsdnet_newproc (char *name, int stacksize, void(*entry)(void *), void *arg);
-#define SIO_RTEMS_SHOW_STATS _IO('i', 250)
 
 #define	MVE643XX_DUMMY_PHY 0 /* phy is defined by low-level driver */
 
